d163a3a84fa555579aafe069f2aad3b3de431638
Jefferson Kalffman de Sousa Lacerda <jefferson@kalffman.com>
2021-01-31 20:29:06 -0400

    feat: First commit

---
+++ src/test/java/com/kalffman/projetos/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalffman.projetos.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
